db0f0677a80fd07d7362d620879084a3466c1840
Jazz <jcronin@unca.edu>
2023-03-28 11:49:23 -0400

    3/28

--- course-website/course-files/tutorials/tutorial07/03-dyslexia-mode/index.js
+++ course-website/course-files/tutorials/tutorial07/03-dyslexia-mode/index.js
@@ -1,4 +1,5 @@
 /* 
   See Smashing Magazine Tutorial:
   https://www.smashingmagazine.com/2021/11/dyslexia-friendly-mode-website/
+  go to course files readings: Smashing-Magazine-Dyslexia
 */
--- course-website/course-files/tutorials/tutorial07/solutions/01-font-switcher/index.js
+++
@@ -1,9 +0,0 @@
-function makeBigger() {
-    document.querySelector('.content').style.fontSize = '3em';
-    document.querySelector('h1').style.fontSize = '3em';
-};
-
-function makeSmaller() {
-    document.querySelector('.content').style.fontSize = '1.4em';
-    document.querySelector('h1').style.fontSize = '2em';
-};
